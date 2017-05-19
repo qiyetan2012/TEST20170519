9552b18677b22bd793950535ba641c2a036c4c27
ndb <ndb@ndbdeMacBook-Pro.local>
2017-05-19 11:54:32 +0800

    xx

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ndb on 17/5/19.
+//  Copyright © 2017年 ndb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ndb on 17/5/19.
+//  Copyright © 2017年 ndb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
